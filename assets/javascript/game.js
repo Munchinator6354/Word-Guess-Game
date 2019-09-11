fd14cc2d310c2497a38edc75c63ff4d17deef333
Ryan Isaacson <munchinator6354@gmail.com>
2019-09-11 14:37:37 -0700

    added link to game.js on index.html at bottom of body. Peudocoded beginning structure of game. Created array of puzzleAnswers.

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//
+//1) Create an array of words
+var puzzleAnswers = ["morpheus", "trinity", "mr anderson", "bend the spoon", "architect"];
+
+//2) Choose a word at random
+//3) Display word as underscores in hangmanWord div
+//4) Get user inputer (letters only)
+//5) Determine if guessed letter is correct
+//6) If Correct, push to hangmanWord array
+//7) If wrong, push to guessed letter array
+
+
+
