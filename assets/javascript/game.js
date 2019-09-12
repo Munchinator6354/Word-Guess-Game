9c020ae0e8124334d3d21535fdff39f38f2d0761
Ryan Isaacson <munchinator6354@gmail.com>
2019-09-11 17:39:18 -0700

    Added functions to select a puzzle answer from array of answers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
-//
+
 //1) Create an array of words
-var puzzleAnswers = ["morpheus", "trinity", "mr anderson", "there is no spoon", "architect", "bullet time", "the twins", "blue pill or red pill", "the oracle", "neo is the one"];
+var puzzleAnswers = ["morpheus", "trinity", "mr anderson", "there is no spoon", "architect", "bullet time", "the twins", "blue pill or red pill", "the oracle", "neo is the one", "the merovingian", "the trainman"];
 
 //2) Choose a word at random
-
+var randomPuzzleAnswer = puzzleAnswers[Math.floor(Math.random()*puzzleAnswers.length)];
 
 //3) Display word as underscores in hangmanWord div
 //4) Get user inputer (letters only)
