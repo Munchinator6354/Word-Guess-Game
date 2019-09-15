76f8bdb062f5ea5e9c615de044b318a71c41a7ee
Ryan Isaacson <munchinator6354@gmail.com>
2019-09-15 16:00:20 -0700

    worked with tutor, made more progress on getting underscores to appear and other <p> tags filled in the html from the javascript code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,79 @@
 //1) Create an array of words
 var computerGuess = ["a"];
 var guessedLettersArray = []
+var wins = 0;
+var guessesLeft = 10;
+var lettersGuessed = [];
+var puzzleAnswers = ["morpheus", "trinity", "architect", "neo", "merovingian", "trainman"];
 
 
-// var puzzleAnswers = ["morpheus", "trinity", "mr anderson", "there is no spoon", "architect", "bullet time", "the twins", "blue pill or red pill", "the oracle", "neo is the one", "the merovingian", "the trainman"];
 
 //2) Choose a word at random and store the answer in a variable
-// var randomPuzzleAnswer = puzzleAnswers[Math.floor(Math.random()*puzzleAnswers.length)];
+function startGame() {
+    guessesLeft = 10;
+
+    var randomPuzzleAnswer = puzzleAnswers[Math.floor(Math.random() * puzzleAnswers.length)];
+
+    var blanks = [];
+
+    var splitAnswer = randomPuzzleAnswer.split("");
+
+    for (i = 0; i < splitAnswer.length; i++) {
+        blanks.push("_");
+    }
+
+    document.getElementById("currentHangmanWord").innerHTML = blanks.join(" ");
+
+    document.getElementById("wins").innerHTML = "Total Wins = " + wins;
+
+    document.getElementById("remainingGuesses").innerHTML = "# of Guesses Remaining = " + guessesLeft;
+
+    document.getElementById("guesses").innerHTML = "Letters Already Guessed: " + guessedLettersArray;
+
+}
+
+function checkLetters(letter) {
+
+
+}
+
+document.addEventListener("DOMContentLoaded",function(event) {
+    startGame();
+});
+
+document.onkeyup = function(event) {
+    console.log(event)
+
+}
+
+
+
+
+
 
 //3) Display # of wins so far in "wins" id (starts at 0)
-var wins = 0;
+
 
 //4) Display # of guesses left in "remainingGuesses" id (starts at 10)
-var guessesLeft = 10;
+
 
 //5) Display array of letters guessed so far in "guesses" id (starts empty)
-var lettersGuessed 
+
 
 //6) Display randomPuzzleAnswer with either a filled in letter from guesses array or place holder underscores in currentHangmanWord div
 
-document.onkeyup = function(event) {
-    var userGuess = event.key
-    if (userGuess == computerGuess ) {
-        console.log('this if statement is working')
-        var used = guessedLettersArray.push(userGuess);
-        console.log(guessedLettersArray);
-        document.getElementById("guesses").innerHTML = "Letters Already Guessed: " + guessedLettersArray;
-    }
-    else {
-        
-    }
-}
+// document.onkeyup = function (event) {
+//     var userGuess = event.key
+//     if (userGuess == computerGuess) {
+//         console.log('this if statement is working')
+//         var used = guessedLettersArray.push(userGuess);
+//         console.log(guessedLettersArray);
+//         document.getElementById("guesses").innerHTML = "Letters Already Guessed: " + guessedLettersArray;
+//     }
+//     else {
+
+//     }
+// }
 
 
 
